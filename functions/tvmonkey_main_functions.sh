becf5cef2b6dcd46def9ca4b2f17e75dcd65a469
Chris Blake <cjb.blake@gmail.com>
2013-02-10 11:44:02 +0000

    Fix to Issue 1. Shows are now cycled per download.

--- functions/tvmonkey_main_functions.sh
+++ functions/tvmonkey_main_functions.sh
@@ -37,7 +37,7 @@ function main ()
 
 	# This function is used to invoke XBMC into rescanning its library so that the new shows
 	# are picked up and can be watched.
-	xbmc_updater
+	#xbmc_updater
 
 	# Send an email to let us know that there are new TV Shows available (if applicable)
 	mail
@@ -71,7 +71,7 @@ function main_cron ()
 
 	# This function is used to invoke XBMC into rescanning its library so that the new shows
 	# are picked up and can be watched.
-	xbmc_updater_cron
+	#xbmc_updater_cron
 
 	# Send an email to let us know that there are new TV Shows available (if applicable)
 	mail_cron
@@ -112,7 +112,7 @@ function main_web ()
 
 	# This function is used to invoke XBMC into rescanning its library so that the new shows
 	# are picked up and can be watched.
-	xbmc_updater_web
+	#xbmc_updater_web
 
 	# Send an email to let us know that there are new TV Shows available (if applicable)
 	mail_web
@@ -157,7 +157,7 @@ function test_web ()
 
 	# This function is used to invoke XBMC into rescanning its library so that the new shows
 	# are picked up and can be watched.
-	xbmc_updater_web_test
+	#xbmc_updater_web_test
 
 	# Send an email to let us know that there are new TV Shows available (if applicable)
 	mail_web_test
@@ -170,51 +170,52 @@ function test_web ()
 	exit 0
 }
 
+
 function show_mover_cron ()
 {
-	
-#Read in the list of defined TV shows line by line
-cat $TVSHOWS | while read line1
+
+cat downloads.tmp | while read DOWNLOAD
 do
-#For each TV show (line1) create a number of variables
-#The name of the TV show
-SHOW=${line1%:*}
-SHOW_N=$(echo $SHOW | tr . " ")
 
-#The length of the show name - Used to find where the season number begins
-SHOW_L=${#SHOW}
-SHOW_L=$SHOW_L+2;
+	#Read in the list of defined TV shows line by line
+	cat $TVSHOWS | while read line1
+	do
+	
+	#For each TV show (line1) create a number of variables
+	#The name of the TV show
+	SHOW=${line1%:*}
+	SHOW_N=$(echo $SHOW | tr . " ")
 
-#After the delimiter, the location where we want this show to be stored.
-LOC=${line1##*:}
+	#The length of the show name - Used to find where the season number begins
+	SHOW_L=${#SHOW}
+	SHOW_L=$SHOW_L+2;
 
-    #Cycle through the downloaded files trying to match to a TV show we have defined.
-    cat downloads.tmp | while read DOWNLOAD
-    do
-    if [[ $DOWNLOAD == $SHOW.S* ]] ; then
+	#After the delimiter, the location where we want this show to be stored.
+	LOC=${line1##*:}
+
+	if [[ $DOWNLOAD == $SHOW.S* ]] ; then
 	#If we find one then extract the season number
 	season=${DOWNLOAD:$SHOW_L:2}	
-	
-		#Move the file to the Correct folder depending on which Season it is in.
-		case $season in
-		0[1-9])
-			i=$(echo $season | cut -c 2)			
-			if [ -d "$LOC/Season $i" ]; then
+	#Move the file to the Correct folder depending on which Season it is in.
+	case $season in
+	0[1-9])
+		i=$(echo $season | cut -c 2)			
+		if [ -d "$LOC/Season $i" ]; then
+			mv $DOWNLOAD "$LOC/Season $i";
+			x=$(echo $DOWNLOAD >> message.txt)
+			#Set a marker to show we have done something
+			maildo=$(echo '1' >> maildo)
+		else 
+			if [ ! -d "$LOC/Season $i" ]; then
+				mkdir "$LOC/Season $i";
 				mv $DOWNLOAD "$LOC/Season $i";
 				x=$(echo $DOWNLOAD >> message.txt)
 				#Set a marker to show we have done something
 				maildo=$(echo '1' >> maildo)
-			else 
-				if [ ! -d "$LOC/Season $i" ]; then
-					mkdir "$LOC/Season $i";
-					mv $DOWNLOAD "$LOC/Season $i";
-					x=$(echo $DOWNLOAD >> message.txt)
-					#Set a marker to show we have done something
-					maildo=$(echo '1' >> maildo)
-				fi
 			fi
+		fi
 		;;
-		esac
+	esac
     fi
 	
 	if [[ $DOWNLOAD =~ $SHOW.[1-9]* ]] ; then
@@ -223,82 +224,85 @@ LOC=${line1##*:}
 	season=${DOWNLOAD:$SHOW_L:1}	
 		
 		#Move the file to the Correct folder depending on which Season it is in.
-		case $season in
-		[1-9])
-			i=$(echo $season)			
-			if [ -d "$LOC/Season $i" ]; then
+	case $season in
+	[1-9])
+		i=$(echo $season)			
+		if [ -d "$LOC/Season $i" ]; then
+			mv $DOWNLOAD "$LOC/Season $i";
+			x=$(echo $DOWNLOAD >> message.txt)
+			#Set a marker to show we have done something
+			maildo=$(echo '1' >> maildo)
+		else 
+			if [ ! -d "$LOC/Season $i" ]; then
+				mkdir "$LOC/Season $i";
 				mv $DOWNLOAD "$LOC/Season $i";
 				x=$(echo $DOWNLOAD >> message.txt)
 				#Set a marker to show we have done something
 				maildo=$(echo '1' >> maildo)
-			else 
-				if [ ! -d "$LOC/Season $i" ]; then
-					mkdir "$LOC/Season $i";
-					mv $DOWNLOAD "$LOC/Season $i";
-					x=$(echo $DOWNLOAD >> message.txt)
-					#Set a marker to show we have done something
-					maildo=$(echo '1' >> maildo)
-				fi
 			fi
-		;;
+		fi
+	;;
 		esac
     fi	
-    done
-
-# Reset the show length counter back to 0 to fix a bug [0000001] which prevented multiple show moves in one run of TVMonkey.
-SHOW_L=0;
-SHOW_N="";
+    
+    # Reset the show length counter back to 0 to fix a bug [0000001] which prevented multiple show moves in one run of TVMonkey.
+	SHOW_N="";
+    SHOW_L=0;
+    
+	done
 
 done
-
+	
 }
+
 
 function show_mover ()
 {
 	
-#Read in the list of defined TV shows line by line
-cat $TVSHOWS | while read line1
+cat downloads.tmp | while read DOWNLOAD
 do
-#For each TV show (line1) create a number of variables
-#The name of the TV show
-SHOW=${line1%:*}
-SHOW_N=$(echo $SHOW | tr . " ")
 
-#The length of the show name - Used to find where the season number begins
-SHOW_L=${#SHOW}
-SHOW_L=$SHOW_L+2;
+	#Read in the list of defined TV shows line by line
+	cat $TVSHOWS | while read line1
+	do
+	
+	#For each TV show (line1) create a number of variables
+	#The name of the TV show
+	SHOW=${line1%:*}
+	SHOW_N=$(echo $SHOW | tr . " ")
 
-#After the delimiter, the location where we want this show to be stored.
-LOC=${line1##*:}
+	#The length of the show name - Used to find where the season number begins
+	SHOW_L=${#SHOW}
+	SHOW_L=$SHOW_L+2;
 
-    #Cycle through the downloaded files trying to match to a TV show we have defined.
-    cat downloads.tmp | while read DOWNLOAD
-    do
-    if [[ $DOWNLOAD == $SHOW.S* ]] ; then
+	#After the delimiter, the location where we want this show to be stored.
+	LOC=${line1##*:}
+
+	if [[ $DOWNLOAD == $SHOW.S* ]] ; then
 	#If we find one then extract the season number
-	season=${DOWNLOAD:$SHOW_L:2}		
-		#Move the file to the Correct folder depending on which Season it is in.
-		case $season in
-		0[1-9])
-			i=$(echo $season | cut -c 2)			
-			if [ -d "$LOC/Season $i" ]; then
+	season=${DOWNLOAD:$SHOW_L:2}	
+	#Move the file to the Correct folder depending on which Season it is in.
+	case $season in
+	0[1-9])
+		i=$(echo $season | cut -c 2)			
+		if [ -d "$LOC/Season $i" ]; then
+			echo "$DOWNLOAD is from $SHOW_N. Moving it to the Season $i folder..."
+			mv $DOWNLOAD "$LOC/Season $i";
+			x=$(echo $DOWNLOAD >> message.txt)
+			#Set a marker to show we have done something
+			maildo=$(echo '1' >> maildo)
+		else 
+			if [ ! -d "$LOC/Season $i" ]; then
+				mkdir "$LOC/Season $i";
 				echo "$DOWNLOAD is from $SHOW_N. Moving it to the Season $i folder..."
 				mv $DOWNLOAD "$LOC/Season $i";
 				x=$(echo $DOWNLOAD >> message.txt)
 				#Set a marker to show we have done something
 				maildo=$(echo '1' >> maildo)
-			else 
-				if [ ! -d "$LOC/Season $i" ]; then
-					mkdir "$LOC/Season $i";
-					echo "$DOWNLOAD is from $SHOW_N. Moving it to the Season $i folder..."
-					mv $DOWNLOAD "$LOC/Season $i";
-					x=$(echo $DOWNLOAD >> message.txt)
-					#Set a marker to show we have done something
-					maildo=$(echo '1' >> maildo)
-				fi
 			fi
+		fi
 		;;
-		esac
+	esac
     fi
 	
 	if [[ $DOWNLOAD =~ $SHOW.[1-9]* ]] ; then
@@ -307,106 +311,35 @@ LOC=${line1##*:}
 	season=${DOWNLOAD:$SHOW_L:1}	
 		
 		#Move the file to the Correct folder depending on which Season it is in.
-		case $season in
-		[1-9])
-			i=$(echo $season)			
-			if [ -d "$LOC/Season $i" ]; then
+	case $season in
+	[1-9])
+		i=$(echo $season)			
+		if [ -d "$LOC/Season $i" ]; then
+			echo "$DOWNLOAD is from $SHOW_N. Moving it to the Season $i folder..."
+			mv $DOWNLOAD "$LOC/Season $i";
+			x=$(echo $DOWNLOAD >> message.txt)
+			#Set a marker to show we have done something
+			maildo=$(echo '1' >> maildo)
+		else 
+			if [ ! -d "$LOC/Season $i" ]; then
 				echo "$DOWNLOAD is from $SHOW_N. Moving it to the Season $i folder..."
+				mkdir "$LOC/Season $i";
 				mv $DOWNLOAD "$LOC/Season $i";
 				x=$(echo $DOWNLOAD >> message.txt)
 				#Set a marker to show we have done something
 				maildo=$(echo '1' >> maildo)
-			else 
-				if [ ! -d "$LOC/Season $i" ]; then
-					mkdir "$LOC/Season $i";
-				    echo "$DOWNLOAD is from $SHOW_N. Moving it to the Season $i folder..."
-					mv $DOWNLOAD "$LOC/Season $i";
-					x=$(echo $DOWNLOAD >> message.txt)
-					#Set a marker to show we have done something
-					maildo=$(echo '1' >> maildo)
-				fi
 			fi
-		;;
+		fi
+	;;
 		esac
-    fi
-	
-    done
-done
+    fi	
+    
+    # Reset the show length counter back to 0 to fix a bug [0000001] which prevented multiple show moves in one run of TVMonkey.
+	SHOW_N="";
+    SHOW_L=0;
+    
+	done
 
-}
-
-function show_mover_web_test ()
-{
-	
-#Read in the list of defined TV shows line by line
-cat $TVSHOWS | while read line1
-do
-#For each TV show (line1) create a number of variables
-#The name of the TV show
-SHOW=${line1%:*}
-SHOW_N=$(echo $SHOW | tr . " ")
-
-#The length of the show name - Used to find where the season number begins
-SHOW_L=${#SHOW}
-SHOW_L=$SHOW_L+2;
-
-#After the delimiter, the location where we want this show to be stored.
-LOC=${line1##*:}
-
-    #Cycle through the downloaded files trying to match to a TV show we have defined.
-    cat downloads.tmp | while read DOWNLOAD
-    do
-    if [[ $DOWNLOAD == $SHOW.S* ]] ; then
-	#If we find one then extract the season number
-	season=${DOWNLOAD:$SHOW_L:2}		
-		#Move the file to the Correct folder depending on which Season it is in.
-		case $season in
-		0[1-9])
-			i=$(echo $season | cut -c 2)			
-			if [ -d "$LOC/Season $i" ]; then
-				echo "$DOWNLOAD is from $SHOW_N. would move it to the Season $i folder..."
-				x=$(echo $DOWNLOAD >> message.txt)
-				#Set a marker to show we have done something
-				maildo=$(echo '1' >> maildo)
-			else 
-				if [ ! -d "$LOC/Season $i" ]; then
-					echo "$DOWNLOAD is from $SHOW_N. Would move it to the Season $i folder..."
-					x=$(echo $DOWNLOAD >> message.txt)
-					#Set a marker to show we have done something
-					maildo=$(echo '1' >> maildo)
-				fi
-			fi
-		;;
-		esac
-    fi
-	
-	if [[ $DOWNLOAD =~ $SHOW.[1-9]* ]] ; then
-	#If we find one then extract the season number
-	SHOW_L=$SHOW_L-1;
-	season=${DOWNLOAD:$SHOW_L:1}	
-		
-		#Move the file to the Correct folder depending on which Season it is in.
-		case $season in
-		[1-9])
-			i=$(echo $season)			
-			if [ -d "$LOC/Season $i" ]; then
-				echo "$DOWNLOAD is from $SHOW_N. Would move it to the Season $i folder..."
-				x=$(echo $DOWNLOAD >> message.txt)
-				#Set a marker to show we have done something
-				maildo=$(echo '1' >> maildo)
-			else 
-				if [ ! -d "$LOC/Season $i" ]; then
-				    echo "$DOWNLOAD is from $SHOW_N. Would move it to the Season $i folder..."
-					x=$(echo $DOWNLOAD >> message.txt)
-					#Set a marker to show we have done something
-					maildo=$(echo '1' >> maildo)
-				fi
-			fi
-		;;
-		esac
-    fi
-	
-    done
 done
 
 }
